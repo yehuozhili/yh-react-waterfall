41094522c6c87810e0ad9c74740d81cad485efdb
yehuozhili <34372936+yehuozhili@users.noreply.github.com>
2020-10-11 18:09:40 +0000

    Deploying to gh-pages from  @ 680aa3b3cf602b2952515877b8459b962b6e1a89 🚀

---
+++ precache-manifest.ae9e637b0e130eb90ee8892b346ecc30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8eff7ab0e4e128b020abd03069d0dc6",
+    "url": "/yh-react-waterfall/index.html"
+  },
+  {
+    "revision": "329446658ac67f6ac980",
+    "url": "/yh-react-waterfall/static/js/2.00959d45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yh-react-waterfall/static/js/2.00959d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc956387185fabe97550",
+    "url": "/yh-react-waterfall/static/js/main.a7b5c465.chunk.js"
+  },
+  {
+    "revision": "5c4bbddc717726b37bb8",
+    "url": "/yh-react-waterfall/static/js/runtime-main.48ed0f3c.js"
+  }
+]);
